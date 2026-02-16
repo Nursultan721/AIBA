d27ecb9d08967ae5ec19ac443aa50b5e9b868e6d
Nursultan721 <avazalievnursultan13@gmail.com>
2026-02-16 20:30:48 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './widgets/header/Header'
+import Banner from './widgets/banner/Banner'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Banner />
+    </>
+  )
+}
+
+export default App
---
+++ src/widgets/banner/Banner.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Banner() {
+  return (
+    <div className='banner'>
+        <h1 className='banner__title'>Араван Илим Билим Академия</h1>
+        <p className='banner__text'>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+    </div>
+  )
+}
