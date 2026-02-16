6f1a84ee17ce49cd105ddecb8a6775d456ffb835
Nursultan721 <avazalievnursultan13@gmail.com>
2026-02-16 22:01:12 +0600

    Обновил проект: баннер, хедер, стили

--- src/widgets/header/Header.jsx
+++ src/widgets/header/Header.jsx
@@ -2,20 +2,23 @@ import React from 'react'
 import logo from '../../assets/headerLogo.png'
 import glass from '../../assets/glass.png'
 import language from '../../assets/language.png'
+import './header.scss'
 
 export default function Header() {
   return (
     <header className='header'>
         <img src={logo} alt="Logo" />
-        <nav>
+        <nav className='header__nav'>
             <a href="#about">О нас</a>
             <a href="#college">Колледж</a>
             <a href="#school">Школа</a>
             <a href="#kinderGarden">Детский сад</a>
             <a href="#awards">Награды</a>
         </nav>
-        <img src={glass} alt="Glass" />
-        <img src={language} alt="Language" />
+        <div className='header__buttons'>
+        <button className='header__buttons-btn'><img src={glass} alt="Glass" /></button>
+        <button className='header__buttons-btn'><img src={language} alt="Language" /></button>
+        </div>
     </header>
   )
 }
